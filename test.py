713b1d77ba3182bfe0aab26e41db0f3948f4cbad
Catalin Vasiliu <catalin.vasiliu@evozon.com>
2021-03-13 10:41:38 +0200

    ??

--- test.py
+++ test.py
@@ -1,32 +1,36 @@
-# from amp_iot.src.amp.app import AmpApp
-# #
-# #
-# # p = AmpApp.get_object_manager().get('amp_iot.src.amp.driver.preamp.Preamp')
-# # server = Server(AmpApp, 'localhost', 7004, 0)
-# # server.start()
-# #
-# data = {
-#     'method_name': 'setMainVolume',
-#     'args': {0: 4}
-# }
-#
-# rd = {'path': 'mopidy/audio_option', 'data': data}
-# AmpApp.run(rd)
-#
-# from amp_iot.src.lib.framework.client import Client
-# c = Client('localhost', 7020)
-# data = {
-#     'method_name': 'getMainVolume',
-#     'args': {}
-# }
-# c.send('mopidy/audio_option', data)
+from amp_iot.src.amp.driver.power_led import PowerLed
+
+l=PowerLed()
 
 
+from amp_iot.src.amp.driver.key import Key
+
+k=Key()
+k.start()
+
+
+#
 from amp_iot.src.amp.main import AmplifierMain
 
 amp = AmplifierMain()
 
 amp.run()
 
-a=3
+from amp_iot.src.amp.driver.power_led import PowerLed
+p = PowerLed()
+p.set_red_on()
 
+from amp_iot.src.amp.driver.power import Power
+p=Power()
+
+
+import sounddevice as sd
+
+duration = 10  # seconds
+
+def print_sound(indata, outdata, frames, time, status):
+    volume_norm = indata
+    print (int(volume_norm))
+
+with sd.Stream(callback=print_sound):
+    sd.sleep(duration * 1000)
